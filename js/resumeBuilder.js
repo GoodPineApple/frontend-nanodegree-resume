82a0c20aa58f00a2979136915fc46a308a0bd885
Taemin Kim <taemin3000@naver.com>
2017-07-25 18:39:42 +0900

    proceed udacity datatypes..

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,23 @@
-$("#name").append("Good PineApple");
+var myName = "김태민"
+var myRole = "웹 개발자"
+var formattedName = HTMLheaderName.replace("%data%",myName);
+$('#header').prepend(formattedName);
+var formattedRole = HTMLheaderRole.replace('%data%',myRole);
+$('#header').prepend(formattedRole);
+
+var sampleArray = [0,0,7];
+
+function incrementLastArrayElement(_array)  {
+    var newArray = [];
+    newArray = _array.slice(0);
+    console.log(newArray);
+    var lastNumber = newArray.pop();
+    console.log(newArray);
+    console.log(lastNumber);
+    newArray.push(lastNumber + 1);
+    console.log(newArray);
+    return newArray;
+}
+console.log(incrementLastArrayElement(sampleArray));
+
+
