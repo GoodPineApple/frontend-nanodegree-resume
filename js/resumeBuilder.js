72a89e645603f73ea4a1d4ca8b212f9a2edec67a
Taemin Kim <taemin3000@naver.com>
2017-07-27 17:55:56 +0900

    fill bio,edu,work for learning

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,5 +6,31 @@ var formattedRole = HTMLheaderRole.replace('%data%',myRole);
 $('#header').prepend(formattedRole);
 
 // work, school, bio 객체표기법으로 객체생성해놓기.
+var education = {
+    "schools": [
+        {
+            "name": "구로고등학교",
+            "degree" : "졸업"
+        },
+        {
+            "name": "성공회대학교",
+            "degree": "학사졸업"
+        }
+    ]
+}
 
+var work = {
+    "company": [
+        {
+            "name": "성공회대학교 인문학습원",
+            "years": 1.5
+        }
+    ]
+}
+
+var bio = {
+    "name": "김태민",
+    "age": 25,
+    "skills": ["java", "MySQL", "Spring", "python"]
+}
 
